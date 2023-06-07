c4dbe055baa16a571cd453b57d5b029ba7e611b8
shtxkrxl <sanya.kalmaev@yandex.ru>
2023-06-07 18:04:30 +0500

    fix config to access images from kinopoisk

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'st.kp.yandex.net',
+        port: '',
+        pathname: '/images/**',
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
