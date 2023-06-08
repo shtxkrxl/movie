451ada61984b77f5cca45c37ff71cbfbe51d8274
shtxkrxl <sanya.kalmaev@yandex.ru>
2023-06-08 10:05:13 +0500

    fix: 0 rating now does not display

--- src/app/components/MovieCard.tsx
+++ src/app/components/MovieCard.tsx
@@ -15,20 +15,22 @@ const MovieCard = ({ size, movie }: Props) => {
         className='pointer-events-none h-full w-full'
       />
 
-      <div
-        className={`${
-          size === 'lg'
-            ? 'text-[20px] leading-[28px]'
-            : 'text-[16px] leading-[24px]'
-        } ${
-          movie.rating.kp > 6
-            ? 'bg-[#00B631]'
-            : movie.rating.kp > 4
-            ? 'bg-[#DEA000]'
-            : 'bg-[#CA0000]'
-        } absolute left-[8px] top-[8px] px-[6px]  font-semibold`}>
-        {Math.round(movie.rating.kp * 10) / 10}
-      </div>
+      {movie.rating.kp > 0 && (
+        <div
+          className={`${
+            size === 'lg'
+              ? 'text-[20px] leading-[28px]'
+              : 'text-[16px] leading-[24px]'
+          } ${
+            movie.rating.kp > 6
+              ? 'bg-[#00B631]'
+              : movie.rating.kp > 4
+              ? 'bg-[#DEA000]'
+              : 'bg-[#CA0000]'
+          } absolute left-[8px] top-[8px] px-[6px]  font-semibold`}>
+          {Math.round(movie.rating.kp * 10) / 10}
+        </div>
+      )}
     </div>
   );
 };
