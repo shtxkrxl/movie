f5cf75e8f61255d450c1471ceba70082a16149e0
shtxkrxl <sanya.kalmaev@yandex.ru>
2023-06-08 13:12:53 +0500

    create Error component

---
+++ src/app/components/Error.tsx
@@ -0,0 +1,9 @@
+const Error = () => {
+  return (
+    <div className='flex items-center justify-center text-[32px] text-[#BB2649]'>
+      Something went wrong...
+    </div>
+  );
+};
+
+export default Error;
