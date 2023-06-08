342f704b6e7457cd425832accb34fafcff78f4ed
shtxkrxl <sanya.kalmaev@yandex.ru>
2023-06-08 19:11:28 +0500

    upgrade navbar

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,19 +1,24 @@
 import { Bookmark, Clapperboard } from 'lucide-react';
 import Search from './Search';
+import Link from 'next/link';
 
 const Navbar = () => {
   return (
-    <div className='flex h-[80px] items-center justify-between px-[130px]'>
-      <div className='flex cursor-pointer items-center transition-all hover:text-[#BB2649] active:scale-[1.1] '>
-        <h1 className='text-[40px] font-bold tracking-widest'>MOVIE</h1>
-        <Clapperboard color='#BB2649' size={45} strokeWidth={1} />
-      </div>
+    <div className='sticky top-0 flex h-[70px] w-full items-center justify-between bg-gradient-to-b from-black to-black/50 px-[130px] backdrop-blur-[2px]'>
+      <Link href={'/'}>
+        <div className='flex cursor-pointer items-center transition-all hover:text-[#BB2649] active:scale-[1.1] '>
+          <h1 className='select-none text-[40px] font-bold tracking-widest'>
+            MOVIE
+          </h1>
+          <Clapperboard color='#BB2649' size={45} strokeWidth={1} />
+        </div>
+      </Link>
 
       <div className='flex items-center gap-[15px]'>
         <Search />
         <Bookmark
           color='white'
-          size={45}
+          size={35}
           strokeWidth={1}
           className='cursor-pointer transition-all hover:stroke-[#BB2649] active:scale-[1.1] '
         />
--- src/app/components/Search.tsx
+++ src/app/components/Search.tsx
@@ -2,17 +2,17 @@ import { Dices } from 'lucide-react';
 
 const Search = () => {
   return (
-    <div className='relative h-[40px] w-[400px]'>
+    <div className='relative h-[35px] w-[350px]'>
       <input
         className='h-full w-full rounded-[10px] px-[10px] text-[20px] font-medium text-black outline-none transition-all placeholder:text-[#5B5B5B] focus:outline-[2px] focus:outline-offset-2 focus:outline-[#BB2649]'
         type='text'
         placeholder='Поиск'
       />
       <Dices
-        size={35}
+        size={30}
         strokeWidth={2}
         color='#5B5B5B'
-        className='absolute right-[5px] top-[2px] cursor-pointer transition-all hover:stroke-[#BB2649] active:scale-[1.1]'
+        className='absolute right-[5px] top-[3px] cursor-pointer transition-all hover:stroke-[#BB2649] active:scale-[1.1]'
       />
     </div>
   );
