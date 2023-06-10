08fcb2b3205f96b9496adce537c5b6288d24526c
shtxkrxl <sanya.kalmaev@yandex.ru>
2023-06-10 14:39:02 +0500

    add responsive design

--- src/app/components/MovieCard.tsx
+++ src/app/components/MovieCard.tsx
@@ -1,37 +1,42 @@
 import Image from 'next/image';
-import { Movie } from '../models/models';
+import { Movie } from '../models/movieList';
+import Link from 'next/link';
 
 const MovieCard = ({ size, movie }: Props) => {
   return (
-    <div
-      className={`${
-        size === 'lg' ? 'h-[386px] w-[269px]' : 'h-[274px] w-[191px]'
-      } relative cursor-pointer transition-all hover:z-[1] hover:scale-[1.1] hover:shadow-[0_0_11px_20px_rgba(0,0,0,0.25)]`}>
-      <Image
-        src={movie.poster.previewUrl}
-        alt='Movie poster'
-        width={269}
-        height={386}
-        className='pointer-events-none h-full w-full'
-      />
+    <Link href={`/movie/${movie.id}`}>
+      <div
+        className={`${
+          size === 'lg'
+            ? 'w-[50vw] sm:w-[25vw] lg:w-[15vw]'
+            : 'w-[40vw] sm:w-[20vw] lg:w-[12vw]'
+        } relative cursor-pointer transition-all hover:z-[1] hover:scale-[1.1] hover:shadow-[0_0_11px_20px_rgba(0,0,0,0.25)]`}>
+        <Image
+          src={movie.poster.previewUrl}
+          alt='Movie poster'
+          width={269}
+          height={386}
+          className='pointer-events-none h-full w-full'
+        />
 
-      {movie.rating.kp > 0 && (
-        <div
-          className={`${
-            size === 'lg'
-              ? 'text-[20px] leading-[28px]'
-              : 'text-[16px] leading-[24px]'
-          } ${
-            movie.rating.kp > 6
-              ? 'bg-[#00B631]'
-              : movie.rating.kp > 4
-              ? 'bg-[#DEA000]'
-              : 'bg-[#CA0000]'
-          } absolute left-[8px] top-[8px] px-[6px]  font-semibold`}>
-          {Math.round(movie.rating.kp * 10) / 10}
-        </div>
-      )}
-    </div>
+        {movie.rating.kp > 0 && (
+          <div
+            className={`${
+              size === 'lg'
+                ? 'text-[20px] leading-[28px]'
+                : 'text-[16px] leading-[24px]'
+            } ${
+              movie.rating.kp > 6
+                ? 'bg-[#00B631]'
+                : movie.rating.kp > 4
+                ? 'bg-[#DEA000]'
+                : 'bg-[#CA0000]'
+            } absolute left-[8px] top-[8px] px-[6px]  font-semibold`}>
+            {Math.round(movie.rating.kp * 10) / 10}
+          </div>
+        )}
+      </div>
+    </Link>
   );
 };
 
