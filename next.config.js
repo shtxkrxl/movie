a7cede696829b3409e1ffb50c6c3c0f0a3cee53e
shtxkrxl <sanya.kalmaev@yandex.ru>
2023-06-10 14:38:32 +0500

    change config to accept images from avatars.mds.yandex.net

--- next.config.js
+++ next.config.js
@@ -8,6 +8,12 @@ const nextConfig = {
         port: '',
         pathname: '/images/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'avatars.mds.yandex.net',
+        port: '',
+        pathname: '/get-kinopoisk-image/**',
+      },
     ],
   },
 };
